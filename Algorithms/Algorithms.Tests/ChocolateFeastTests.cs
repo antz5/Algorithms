e415c103e264851ddfd29346c70b04b51f6f1237
Ananth <antz5@users.noreply.github.com>
2016-12-09 20:38:36 +0530

    Algorithm fpr Chocolate feast problem along with unit tests

---
+++ Algorithms/Algorithms.Tests/ChocolateFeastTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Algorithms.Tests
+{
+    [TestClass]
+    public class ChocolateFeastTests
+    {
+        ChocolateFeast chocolates = new ChocolateFeast();
+
+        [TestMethod]
+        public void NumberOfChocolates()
+        {
+            int chocs = chocolates.NumberOfChocolates(10, 2, 5);
+            Assert.AreEqual(6, chocs);
+            Console.WriteLine("Actual: 6 and Expected: "+chocs);
+        }
+
+        [TestMethod]
+        public void NumberOfChocolates2()
+        {
+            int chocs = chocolates.NumberOfChocolates(12, 4, 4);
+            Assert.AreEqual(3, chocs);
+            Console.WriteLine("Actual: 3 and Expected: " + chocs);
+        }
+
+        [TestMethod]
+        public void NumberOfChocolates3()
+        {
+            int chocs = chocolates.NumberOfChocolates(6, 2, 2);
+            Assert.AreEqual(5, chocs);
+            Console.WriteLine("Actual: 5 and Expected: " + chocs);
+        }
+    }
+}
---
+++ Algorithms/Algorithms/ChocolateFeast.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    /*
+         Problem statement: https://www.hackerrank.com/challenges/chocolate-feast
+    */
+    public class ChocolateFeast
+    {
+        public int NumberOfChocolates(int amount, int costOfChocolate, int chocWrappers)
+        {
+            int N = amount;
+            int C = costOfChocolate;
+            int M = chocWrappers;
+
+            int chocolates = N / C;
+            int totalWrappers = chocolates;
+            while (totalWrappers >= M)
+            {
+                int extraChoc = totalWrappers / M;
+                totalWrappers = totalWrappers % M;
+                totalWrappers += extraChoc;
+                chocolates += extraChoc;
+            }
+
+            return chocolates;
+        }
+    }
+}
