bc455346f93815323d054a9b7daef6de9a984d1b
Ananth <antz5@users.noreply.github.com>
2016-12-09 23:46:57 +0530

    Algorithm to solve https://www.hackerrank.com/challenges/cut-the-sticks problem

---
+++ Algorithms/Algorithms.Tests/SticksTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Algorithms.Tests
+{
+    [TestClass]
+    public class SticksTests
+    {
+        CutTheSticks cutSticks = new CutTheSticks();
+
+        [TestMethod]
+        public void SticksCut()
+        {
+            bool pass = false;
+            int[] input = new int[] { 5, 4, 4, 2, 2, 8 };
+            List<int> result = cutSticks.Sticks(6, input);
+            for (int i = 0; i < result.Count; i++)
+            {
+                if (result.Contains(6) && result.Contains(4) && result.Contains(2) && result.Contains(1))
+                {
+                    pass = true;
+                }
+            }
+
+            Assert.AreEqual(true, pass);
+            Console.WriteLine("Actual: true and expected: " + pass);
+        }
+
+        [TestMethod]
+        public void SticksCut2()
+        {
+            bool pass = false;
+            int[] input = new int[] { 1, 2, 3, 4, 3, 3, 2, 1 };
+
+            List<int> result = cutSticks.Sticks(8, input);
+            for (int i = 0; i < result.Count; i++)
+            {
+                if (result.Contains(8) && result.Contains(6) && result.Contains(4) && result.Contains(1))
+                {
+                    pass = true;
+                }
+            }
+
+            Assert.AreEqual(true, pass);
+            Console.WriteLine("Actual: true and expected: " + pass);
+        }
+    }
+}
---
+++ Algorithms/Algorithms/CutTheSticks.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    /*
+    https://www.hackerrank.com/challenges/cut-the-sticks 
+    */
+    public class CutTheSticks
+    {
+        public List<int> Sticks(int numberOfSticks, int[] lengthOfSticks)
+        {
+            
+            List<int> arr = new List<int>(numberOfSticks);
+            List<int> result = new List<int>();
+
+            for (int k = 0; k < lengthOfSticks.Length; k++)
+            {
+                arr.Add(Int32.Parse(lengthOfSticks[k].ToString()));
+            }
+            while (arr.Count > 0)
+            {
+                int sticksCut = 0;
+                int lengthOfCut = SelectionSort(arr);
+                for (int i = 0; i < arr.Count; i++)
+                {
+                    arr[i] -= lengthOfCut;
+                    sticksCut += 1;
+                }
+                if (sticksCut > 0)
+                   result.Add(sticksCut);
+            }
+            return result;
+        }
+
+        private static int SelectionSort(List<int> arr)
+        {
+            int smallest = -1;
+            int temp = 0;
+            for (int i = 0; i < arr.Count; i++)
+            {
+
+                if (arr[i] > 0)
+                {
+                    temp = arr[i];
+                    for (int j = i; j < arr.Count; j++)
+                    {
+                        if (arr[j] < temp)
+                        {
+                            temp = arr[j];
+                            arr[j] = arr[i];
+                            arr[i] = temp;
+                        }
+                    }
+                }
+                else
+                {
+                    arr.RemoveAt(i);
+                    i--;
+                }
+
+            }
+            if (arr.Count > 0)
+                smallest = arr[0];
+            else
+                smallest = 0;
+            return smallest;
+        }
+    }
+}
