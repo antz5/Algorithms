437b044b6ae1b666fb8a621e9779106794ffe2ff
Ananth <antz5@users.noreply.github.com>
2016-12-09 20:24:13 +0530

    Algorithm for Julius caesor problem along with unit test cases

---
+++ Algorithms/Algorithms.Tests/JuliusCaesor_Tests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Algorithms.Tests
+{
+    [TestClass]
+    public class JuliusCaesorTests
+    {
+
+        Algorithms.JuliusCaesor julius = new Algorithms.JuliusCaesor();
+
+        [TestMethod]
+        public void TestForEncryption()
+        {
+            string result= julius.EncryptionCode("middle-Outz", 2);
+            Assert.AreEqual("okffng-Qwvb", result);
+            Console.WriteLine("Actual: okffng-Qwvb and Expected: "+result);
+        }
+
+        [TestMethod]
+        public void TestForInputNull()
+        {
+            string result = julius.EncryptionCode(null, 1);
+            Assert.AreEqual("The input is either null or empty", result);
+            Console.WriteLine("Actual: The input is either null or empty and expected: "+result);
+        }
+
+        [TestMethod]
+        public void TestForBlankInput()
+        {
+            string result = julius.EncryptionCode(string.Empty, 1);
+            Assert.AreEqual("The input is either null or empty", result);
+            Console.WriteLine("Actual: The input is either null or empty and expected: " + result);
+        }
+
+        [TestMethod]
+        public void TestForZeroRotations()
+        {
+            string result = julius.EncryptionCode("middle-Outz", 0);
+            Assert.AreEqual("middle-Outz", result);
+            Console.WriteLine("Actual: middle-Outz and expected: " + result);
+        }
+
+        [TestMethod]
+        public void TestFor26Rotations()
+        {
+            string result = julius.EncryptionCode("middle-Outz", 26);
+            Assert.AreEqual("middle-Outz", result);
+            Console.WriteLine("Actual: middle-Outz and expected: " + result);
+        }
+    }
+}
---
+++ Algorithms/Algorithms/JuliusCaesor.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    /*
+    Problem statement 
+    https://www.hackerrank.com/challenges/caesar-cipher-1
+    */
+    public class JuliusCaesor
+    {
+        public string EncryptionCode(string input, int rotateByChars)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return "The input is either null or empty";
+            }
+            
+            char[] O = new char[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int AsciiOfA = 'A';
+                int AsciiOfa = 'a';
+                int AsciiOfZ = 'Z';
+                int AsciiOfz = 'z';
+                int AsciiOfChar = input[i];
+
+                if (AsciiOfChar >= AsciiOfA && AsciiOfChar <= AsciiOfZ)
+                {
+                    for (int j = 1; j <= rotateByChars; j++)
+                    {
+                        AsciiOfChar += 1;
+                        if (AsciiOfChar > AsciiOfZ)
+                            AsciiOfChar -= 26;
+                    }
+                    O[i] = Convert.ToChar(AsciiOfChar);
+                }
+                else if (AsciiOfChar >= AsciiOfa && AsciiOfChar <= AsciiOfz)
+                {
+                    for (int j = 1; j <= rotateByChars; j++)
+                    {
+                        AsciiOfChar += 1;
+                        if (AsciiOfChar > AsciiOfz)
+                            AsciiOfChar -= 26;
+                    }
+                    O[i] = Convert.ToChar(AsciiOfChar);
+                }
+                else
+                {
+                    O[i] = input[i];
+                }
+            }
+
+           return DisplayString(O);           
+        }
+
+        static string DisplayString(char[] chars)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < chars.Length; i++)
+            {
+              result.Append(chars[i]);
+            }
+            return result.ToString();
+        }
+    }
+}
