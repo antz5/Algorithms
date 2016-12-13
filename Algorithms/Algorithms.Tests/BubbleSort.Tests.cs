9748390d7b77776ddd3f8a3d8f58afe280665151
Ananth <antz5@users.noreply.github.com>
2016-12-13 18:15:50 +0530

    Bubble sort algorithm with unit tests

---
+++ Algorithms/Algorithms.Tests/BubbleSort.Tests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.Tests
+{
+    [TestClass]
+    public class BubbleSortTests
+    {
+        BubbleSort bubble = new BubbleSort();
+
+        [TestMethod]
+        public void Sort1()
+        {
+            
+            int[] input = new int[] { 25, 38, 15, 185, 192, 10 };
+            int[] sortedItems =  bubble.BubbleSorted(input);
+            int[] expectedItems = new int[] {10, 15, 25, 38, 185, 192 };
+            bool success = false; 
+            for (int i = 0; i < input.Length; i++)
+            {
+                success = false;
+                if (sortedItems[i] == expectedItems[i])
+                    success = true;
+            }
+            Assert.AreEqual(true, success);
+            Console.WriteLine("Actual: true and Expected: " + success);
+        }
+
+        [TestMethod]
+        public void Sort2()
+        {
+
+            int[] input = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100, 120, 125, 190, 500 };
+            int[] sortedItems = bubble.BubbleSorted(input);
+            int[] expectedItems = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100, 120, 125, 190, 500 };
+            bool success = false;
+            for (int i = 0; i < input.Length; i++)
+            {
+                success = false;
+                if (sortedItems[i] == expectedItems[i])
+                    success = true;
+            }
+            Assert.AreEqual(true, success);
+            Console.WriteLine("Actual: true and Expected: " + success);
+        }
+
+        [TestMethod]
+        public void Sort3()
+        {
+
+            int[] input = new int[] {  500,  190, 130, 120, 100, 90, 80, 70, 34, 24, 12, 11, 9, 7, 5, 2, 1};
+            int[] sortedItems = bubble.BubbleSorted(input);
+            int[] expectedItems = new int[] { 1, 2, 5, 7,  9, 11, 12, 24, 34, 70, 80, 90, 100, 120, 130, 190, 500 };
+            bool success = false;
+            for (int i = 0; i < input.Length; i++)
+            {
+                success = false;
+                if (sortedItems[i] == expectedItems[i])
+                    success = true;
+            }
+            Assert.AreEqual(true, success);
+            Console.WriteLine("Actual: true and Expected: " + success);
+        }
+    }
+}
---
+++ Algorithms/Algorithms/BubbleSort.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    /*
+        Bubble Sort:
+        Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping 
+        the adjacent elements if they are in wrong order.
+
+        Design: 
+        [X] create a method which accepts a parameter which is an array of integers
+        [X] outer loop to compare with inner loop elements
+        [X] inner loop 
+        [X] check if outer element is greater than inner elements
+        [X] create a method to swap 2 elements using temporary variable
+        [X] return the sorted array
+     */
+    public class BubbleSort
+    {
+        public int[] BubbleSorted(int[] items)
+        {
+            for (int i = 0; i < items.Length-1; i++)
+            {
+                for (int j = 0; j < items.Length - i- 1; j++)
+                {
+                    if(items[j] > items[j+1])
+                    {
+                        int temp = items[j];
+                        items[j] = items[j+1];
+                        items[j+1] = temp;
+                    }
+                }
+            }
+            return items;
+        }
+
+       
+    }
+}
