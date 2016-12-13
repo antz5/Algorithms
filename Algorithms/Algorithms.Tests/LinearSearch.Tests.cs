6ba4514003abd89d55baaaed4d2b0286c9608d42
Ananth <antz5@users.noreply.github.com>
2016-12-13 13:11:14 +0530

    Linear Search Algorithm and unit tests

---
+++ Algorithms/Algorithms.Tests/LinearSearch.Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.Tests
+{
+    [TestClass]
+    public class LinearSearchTests
+    {
+        LinearSearch search = new LinearSearch();
+
+        [TestMethod]
+        public void KeyPresent()
+        {
+            int position = -1;
+            int[] input = new int[] { 5, 8, 4, 10, 2, 23 };
+            position = search.Search(input, 23);
+
+            Assert.AreEqual(5, position);
+            Console.WriteLine("Actual: 5: and Expected: " + position);
+        }
+
+        [TestMethod]
+        public void KeyNotPresent()
+        {
+            int position = -1;
+                int[] input = new int[] { 1, 2, 3, 4, 3, 3, 2, 1 };
+
+            position = search.Search(input, 23);
+
+            Assert.AreEqual(-1, position);
+            Console.WriteLine("Actual: -1: and Expected: " + position);
+
+        }
+    }
+}
---
+++ Algorithms/Algorithms/LinearSearch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    /*
+     Linear Search:
+        Problem: Given an array arr[] of n elements, write a function to search a given element x in arr[].
+        A simple approach is to do linear search, i.e
+        Start from the leftmost element of arr[] and one by one compare x with each element of arr[]
+        If x matches with an element, return the index.
+        If x doesn’t match with any of elements, return -1.
+     */
+
+        /*
+           Design:
+           
+            [X] create a method which accepts two parameters: array and the key to be searched
+            [X] For-Loop through starting from the first index until the last
+            [X] If-condition to check if the key is present in the array
+            [X] return the position of the array where the key is present else return -1
+         */
+    public class LinearSearch
+    {
+        public int Search(int[] items, int key)
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i] == key)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
