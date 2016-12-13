618c56f7a9aac43f9d44a1a8822a34bab769b997
Ananth <antz5@users.noreply.github.com>
2016-12-13 14:02:21 +0530

    Binary search Algorithm and unit tests

---
+++ Algorithms/Algorithms.Tests/BinarySearch.Tests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.Tests
+{
+    [TestClass]
+    public class BinarySearchTests
+    {
+        BinarySearchIterativeApproach search = new BinarySearchIterativeApproach();
+
+        [TestMethod]
+        public void KeyPresent1()
+        {
+            int position = -1;
+            int[] input = new int[] {10, 15, 25, 38, 48, 185, 192 };
+            position = search.Search(input, 10);
+
+            Assert.AreEqual(0, position);
+            Console.WriteLine("Actual: 0 and Expected: " + position);
+        }
+
+        [TestMethod]
+        public void KeyPresent2()
+        {
+            int position = -1;
+            int[] input = new int[] { 10, 15, 25, 38, 48, 185, 192 };
+            position = search.Search(input, 192);
+
+            Assert.AreEqual(6, position);
+            Console.WriteLine("Actual: 6 and Expected: " + position);
+        }
+
+        [TestMethod]
+        public void KeyPresent3()
+        {
+            int position = -1;
+            int[] input = new int[] { 10, 15, 25, 38, 48, 185, 192 };
+            position = search.Search(input, 185);
+
+            Assert.AreEqual(5, position);
+            Console.WriteLine("Actual: 5 and Expected: " + position);
+        }
+
+
+        [TestMethod]
+        public void KeyPresent4()
+        {
+            int position = -1;
+            int[] input = new int[] { 10, 15, 25, 38, 48, 185, 192 };
+            position = search.Search(input, 25);
+
+            Assert.AreEqual(2, position);
+            Console.WriteLine("Actual:2  and Expected: " + position);
+        }
+
+        [TestMethod]
+        public void KeyPresent5()
+        {
+            int position = -1;
+            int[] input = new int[] { 10, 15, 25, 38, 48, 185, 192 };
+            position = search.Search(input, 38);
+
+            Assert.AreEqual(3, position);
+            Console.WriteLine("Actual:3  and Expected: " + position);
+        }
+
+        [TestMethod]
+        public void KeyNotPresent()
+        {
+            int position = -1;
+                int[] input = new int[] { 10, 15, 25, 38, 48, 185, 192 };
+
+            position = search.Search(input, 23);
+
+            Assert.AreEqual(-1, position);
+            Console.WriteLine("Actual: -1: and Expected: " + position);
+        }
+    }
+}
---
+++ Algorithms/Algorithms/BinarySearch.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    /*
+        Binary Search: 
+        Search a sorted array by repeatedly dividing the search interval in half.
+        Begin with an interval covering the whole array. If the value of the search key is less than
+        the item in the middle of the interval, narrow the interval to the lower half. Otherwise narrow 
+        it to the upper half. Repeatedly check until the value is found or the interval is empty.
+
+        Design:
+        [X] create a Search method accepting parameters such as a sorted input array and the key
+        [X] Compute the mid position of the array using mid = left + right/2
+        [X] Check if the key is found at the mid position
+        [X] if not then check if the key is greater than mid, if yes, start comparing from the right
+        [X] else compare from the left
+        [X] return the index/position of the key found
+     */
+    public class BinarySearchIterativeApproach
+    {
+        public int Search(int[] items, int key)
+        {
+            int left = 0;
+            int right = items.Length - 1;
+            int mid = left + right / 2;
+
+            if (items[mid] == key)
+                return mid;
+            else if (key > items[mid])
+            {
+                while (mid <= right)
+                {
+                    if (items[mid] == key)
+                        return mid;
+                    mid += 1;
+                }
+            }
+            else
+            {
+                while (mid >= left)
+                {
+                    if (items[mid] == key)
+                        return mid;
+                    mid -= 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
