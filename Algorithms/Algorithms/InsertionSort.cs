3101e861251542e7b8c71f8dd483409aed57ecda
Ananth <antz5@users.noreply.github.com>
2016-12-14 15:47:19 +0530

    Insertion Sort Algorithm along with unit tests

---
+++ Algorithms/Algorithms/InsertionSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class InsertionSort
+    {
+        public int[] InsertionSorted(int[] items)
+        {
+            int n = items.Length;
+            for (int i = 1; i < n; i++)
+            {
+                int j = i - 1;
+                int value = items[i];
+                while (j >= 0)
+                {                    
+                    if (value < items[j])
+                    {                        
+                        items[j+1] = items[j];
+                        items[j] = value;
+                    }
+                    j -= 1;
+                }                
+            }
+            return items;
+        }
+    }
+}
