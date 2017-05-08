156791066cc2e3da129347692c78e3bdb7d5db07
Ananth <antz5@users.noreply.github.com>
2017-05-08 12:41:59 +1000

    Remove duplicate characters in a given string

---
+++ Algorithms/Algorithms.Tests/RemoveDuplicateCharactersTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.Tests
+{
+    [TestClass]
+    public class RemoveDuplicateCharactersTests
+    {
+        RemoveDuplicateCharacters removeObject = new RemoveDuplicateCharacters();
+        [TestMethod]
+        public void TestForRemoveDuplicateChars()
+        {
+
+            Assert.AreEqual("CSharpt", removeObject.Remove("CSharpStars"));
+            Console.WriteLine(removeObject.Remove("CSharpStars").Equals("CSharpt"));
+			
+        }
+
+		[TestMethod]
+        public void TestForRemoveDuplicateChars2()
+        {
+            Assert.AreEqual("CN", removeObject.Remove("CNN"));
+            Console.WriteLine(removeObject.Remove("CNN").Equals("CN"));
+
+        }
+
+        [TestMethod]
+        public void TestForRemoveDuplicateChars3()
+        {
+            Assert.AreEqual("Gole", removeObject.Remove("GoOgle"));
+            Console.WriteLine(removeObject.Remove("GoOgle").Equals("Gole"));
+
+
+        }
+
+		[TestMethod]
+        public void TestForRemoveDuplicateChars4()
+        {
+            Assert.AreEqual("Yaho", removeObject.Remove("Yahoo"));
+            Console.WriteLine(removeObject.Remove("Yaho").Equals("Yahoo"));
+        }
+    }
+}
---
+++ Algorithms/Algorithms/RemoveDuplicateCharacters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class RemoveDuplicateCharacters
+    {
+        //Program to remove duplicate characters in a string
+        //checks for duplicate characters both upper and lower case letters using ascii values.
+
+        public string Remove(string inputString)
+        {
+            string result = string.Empty;
+            char s='0';
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (inputString[i] >= 97 && inputString[i] <= 122)
+                {
+                    s = (char)(inputString[i] - 32);
+                }
+                else if(inputString[i] >= 65 && inputString[i] <= 90)
+                {
+                    s = (char)(inputString[i] + 32);
+                }
+                if (result.IndexOf(inputString[i]) == -1)                    
+                {
+                    if (result.IndexOf(s) == -1)
+                        result += inputString[i];
+                }
+            }
+            return result;
+        }
+    }
+}
