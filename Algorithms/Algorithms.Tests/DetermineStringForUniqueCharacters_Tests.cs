d235da96798dc08fd6f761f2c8509f0681ee1f62
Ananth <antz5@users.noreply.github.com>
2017-05-08 17:09:02 +1000

    Determine if the string contains unique characters with unit tests

---
+++ Algorithms/Algorithms.Tests/DetermineStringForUniqueCharacters_Tests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.Tests
+{
+    [TestClass]
+    public class DetermineStringForUniqueCharacters_Tests
+    {
+        DetermineStringForUniqueCharacters unique = new DetermineStringForUniqueCharacters();
+
+        [TestMethod]
+        public void TestForUniqueness1()
+        {
+            bool success = unique.CharsInStringUnique("Alive is awesome");
+            Assert.AreEqual(false, success);
+        }
+
+        [TestMethod]
+        public void TestForUniqueness2()
+        {
+            bool success = unique.CharsInStringUnique("Live for the moment");
+            Assert.AreEqual(false, success);
+        }
+
+        [TestMethod]
+        public void TestForUniqueness3()
+        {
+            bool success = unique.CharsInStringUnique("Alive swum ");
+            Assert.AreEqual(true, success);
+        }
+
+        [TestMethod]
+        public void TestForUniqueness4()
+        {
+            bool success = unique.CharsInStringUnique("Alive org");
+            Assert.AreEqual(true, success);
+        }
+
+        [TestMethod]
+        public void TestForUniqueness5()
+        {
+            bool success = unique.CharsInStringUnique("ab cd ef ghi jk lmn ");
+            Assert.AreEqual(true, success);
+        }
+    }
+}
---
+++ Algorithms/Algorithms/DetermineStringForUniqueCharacters.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class DetermineStringForUniqueCharacters
+    {
+        public bool CharsInStringUnique(string input)
+        {            
+            Dictionary<char, char> dictionary = new Dictionary<char, char>();
+            for(int i=0;i<input.Length;i++)
+            {
+                if (dictionary.Keys.Contains(input[i]))
+                    return false;
+                else
+                {
+                    if(input[i] != ' ')
+                        dictionary.Add(input[i], input[i]);
+                }
+            }
+            return true;
+        }
+    }
+}
