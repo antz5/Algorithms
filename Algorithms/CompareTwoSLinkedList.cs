85e728a6cb116f38276c1e2263a65f48ee1ad498
Ananth <antz5@users.noreply.github.com>
2020-10-07 16:23:23 +1100

    Add files via upload
    Compare two singly linked lists to check if they are equal
    Get Node Value of a singly linked list given the PositionFromTail of a list
    Queries to operated on a Queue data structure

---
+++ Algorithms/CompareTwoSLinkedList.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+
+    class SinglyLinkedListNode
+    {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData)
+        {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    class SinglyLinkedList
+    {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList()
+        {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void InsertNode(int nodeData)
+        {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep, TextWriter textWriter)
+    {
+        while (node != null)
+        {
+            textWriter.Write(node.data);
+
+            node = node.next;
+
+            if (node != null)
+            {
+                textWriter.Write(sep);
+            }
+        }
+    }
+
+    // Complete the CompareLists function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static bool CompareLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2)
+    {
+       List<int> hash1 = new List<int>();
+        List<int> hash2 = new List<int>();
+        int capacityOfHead1 = 0;
+        int capacityOfHead2 = 0;
+
+        while(head1 != null)
+        {
+            hash1.Add(head1.data);
+            capacityOfHead1 += 1;
+            head1 = head1.next;
+        }
+
+        while(head2!=null)
+        {
+            hash2.Add(head2.data);
+            capacityOfHead2 += 1;
+            head2 = head2.next;
+        }
+        return hash1.SequenceEqual(hash2);
+    }
+
+    static void Main(string[] args)
+    {
+        
+        int tests = Convert.ToInt32(Console.ReadLine());
+
+        for (int testsItr = 0; testsItr < tests; testsItr++)
+        {
+            SinglyLinkedList llist1 = new SinglyLinkedList();
+
+            int llist1Count = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < llist1Count; i++)
+            {
+                int llist1Item = Convert.ToInt32(Console.ReadLine());
+                llist1.InsertNode(llist1Item);
+            }
+
+            SinglyLinkedList llist2 = new SinglyLinkedList();
+
+            int llist2Count = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < llist2Count; i++)
+            {
+                int llist2Item = Convert.ToInt32(Console.ReadLine());
+                llist2.InsertNode(llist2Item);
+            }
+
+            bool result = CompareLists(llist1.head, llist2.head);
+            Console.WriteLine("result");
+            Console.WriteLine((result ? 1 : 0));
+        }
+
+        
+    }
+}
---
+++ Algorithms/QueueQueries.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queries = Convert.ToInt32(Console.ReadLine());
+            Queue<int> queue = new Queue<int>();
+            for (int q= 0;q <queries;q++)
+            {
+                var input = Console.ReadLine();
+                var splitInput = input.Split(' ');
+                var value = (Convert.ToInt32(splitInput[0]) == 1)? Convert.ToInt32(splitInput[1]):0;
+                switch(Convert.ToInt32(splitInput[0]))
+                {
+                    case 1:
+                        queue.Enqueue(value);
+                        break;
+                    case 2:
+                        queue.Dequeue();
+                        break;
+                    case 3:
+                        Console.WriteLine(queue.Peek());
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
