cc2b1f4ac0b1d4196719e1848e8920d2bf26a9e4
Ananth <antz5@users.noreply.github.com>
2020-10-08 16:18:12 +1100

    Add files via upload
    Reversing an integer

---
+++ Algorithms/Algorithms/ReverseInteger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ReverseInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var x = Convert.ToInt32(Console.ReadLine());
+            var result = Reverse(x);
+
+            Console.WriteLine(result);
+
+        }
+        public static int Reverse(int x)
+        {
+            int result = 0;
+            while (x != 0)
+            {
+                int remove = x % 10;
+                x = x / 10;
+
+                if (result >= Int32.MaxValue / 10  && remove > 7)
+                    return 0;
+                if (result <= int.MinValue / 10 && result < -8)
+                    return 0;
+                result = result * 10 + remove;
+            }
+           
+            return (int)result;
+
+           
+        }
+    }
+}
