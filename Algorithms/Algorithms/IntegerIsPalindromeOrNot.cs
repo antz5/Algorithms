179562ceb5b9dfb39fc99cf20a831ab0e6626bd3
Ananth <antz5@users.noreply.github.com>
2020-10-09 18:26:49 +1100

    Add files via upload
    Algorithm to check if the given integer is a palindrome or not

---
+++ Algorithms/Algorithms/IntegerIsPalindromeOrNot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello World");
+        var x = Convert.ToInt32(Console.ReadLine());
+        var result = IsPalindrome(x);
+        Console.WriteLine(result);
+    }
+
+    public static bool IsPalindrome(int x)
+    {
+        if (x < 0)
+        {
+            return false;
+        }
+        int input = x;
+        int result = 0;
+
+        while (x != 0)
+        {
+            int pop = x % 10;
+            x = x / 10;
+
+            result = result * 10 + pop;
+        }
+        return (result == input);
+
+    }
+}
